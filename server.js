a8a4cebd713c8c26b6834055cf626e773191b2d1
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-04-09 01:07:15 +0300

    Add basic middleware example

--- server.js
+++ server.js
@@ -13,10 +13,36 @@ const app = express();
 // 	res.send(users);
 // });
 
-app.get('*', (req, res) => {
-	console.log('users requested');
-	res.send('test');
-});
+// app.get('*', (req, res) => {
+// 	console.log('users requested');
+// 	res.send('test');
+// });
+
+app.get(
+	'/test',
+	(req, res, next) => {
+		console.log('first callback');
+
+    req.abracadabra = 'magic trick';
+
+    // функція сигналізує що можна переходити на наступний обробник
+    next();
+	},
+	(req, res, next) => {
+		console.log('second callback');
+		console.log(req.abracadabra);
+
+    if(Math.random() > 0.5) {
+      next();
+    } else {
+      res.send('ERROR happened');
+    }
+	},
+	(req, res) => {
+		console.log('third callback');
+		res.send('all done');
+	}
+);
 
 const PORT = 3000;
 const HOST = 'locahost';
